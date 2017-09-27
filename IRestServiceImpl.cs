31cadc7984b929d40f8a9ab0563be0cd6ca48f8c
David Mitchell <davidbmitchell77@outlook.com>
2017-09-27 06:48:47 -0700

    Add files via upload

---
+++ IRestServiceImpl.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace RestService
+{
+    [ServiceContract]
+    public interface IRestServiceImpl
+    {
+        /*(
+        [OperationContract]
+        [WebInvoke(Method = "GET",
+          ResponseFormat = WebMessageFormat.Xml,
+          BodyStyle = WebMessageBodyStyle.Wrapped,
+          UriTemplate = "xml/{id}")]
+        string XMLData(string id);
+
+        [OperationContract]
+        [WebInvoke(Method = "GET",
+         ResponseFormat = WebMessageFormat.Json,
+         BodyStyle = WebMessageBodyStyle.Wrapped,
+         UriTemplate = "json/{id}")]
+        string JSONData(string id);
+        */
+
+        [OperationContract]
+        [WebInvoke(Method = "GET",
+          ResponseFormat = WebMessageFormat.Json,
+          BodyStyle = WebMessageBodyStyle.Bare,
+          UriTemplate = "pulse")]
+        PCHEmailAPIResponse Pulse();
+
+        [OperationContract]
+        [WebInvoke(Method = "POST",
+          RequestFormat = WebMessageFormat.Json,
+          ResponseFormat = WebMessageFormat.Json,
+          BodyStyle = WebMessageBodyStyle.Bare,
+          UriTemplate = "preview")]
+        PCHEmailAPIResponse Preview(PCHEmailAPI request);
+
+        [OperationContract]
+        [WebInvoke(Method = "POST",
+          RequestFormat = WebMessageFormat.Json,
+          ResponseFormat = WebMessageFormat.Json,
+          BodyStyle = WebMessageBodyStyle.Bare,
+          UriTemplate = "send")]
+        PCHEmailAPIResponse Send(PCHEmailAPI request);
+    }
+
+    public class AssociateProfileNode
+    {
+        public string FirstName;
+        public string LastName;
+        public string Designation;
+        public string FunctionalTitle;
+        public string CorporateTitle;
+        public string Company;
+        public string PhotoURL;
+        public string eMail;
+        public string Address1;
+        public string Address2;
+        public string City;
+        public string State;
+        public string Zip;
+        public string PreferredPhone;
+        public string OptionalExtension;
+        public string BBA_URL;
+        public string NMLS;
+        public string SupervisoryAddr1;
+        public string SupervisoryAddr2;
+        public string SupervisoryCity;
+        public string SupervisoryState;
+        public string SupervisoryZIP;
+        public string SupervisoryPhone;
+        public string CAID;
+        public string BC_ZIP;
+        public string VanityURL;
+    }
+
+    public class AgendaItem
+    {
+        public string Item;
+    }
+
+    public class PCH
+    {
+        public string PCHID;
+        public string AssetTitle;
+        public string ILXURL;
+        public string ILXURLnoMetrics;
+        public string Branding;
+        public string PublishDate;
+        public Boolean IsPersonalized;
+        public string Language;
+    }
+
+    public class AdditionalData
+    {
+        public string Key;
+        public string Type;
+        public string Value;
+    }
+
+    [DataContract]
+    public class PCHEmailAPI
+    {
+        [DataMember] public string EmailID { get; set; }
+        [DataMember] public string PersonNumber { get; set; }
+        [DataMember] public string PlatformName { get; set; }
+        [DataMember] public string PartyId { get; set; }
+        [DataMember] public String TemplateID { get; set; }
+        [DataMember] public string SubjectLine { get; set; }
+        [DataMember] public string ToEmailAddress { get; set; }
+        [DataMember] public List<string> SendACopyList { get; set; }
+        [DataMember] public AssociateProfileNode AssociatePersonalization;
+        [DataMember] public List<AgendaItem> AgendaItems { get; set; }
+        [DataMember] public string Appointment { get; set; }
+        [DataMember] public string Meeting { get; set; }
+        [DataMember] public List<PCH> PCHList { get; set; }
+        [DataMember] public List<AdditionalData> AdditionalData { get; set; }
+    }
+
+    [DataContract]
+    public class PCHEmailAPIResponse
+    {
+        [DataMember] public int responseCode { get; set; }
+        [DataMember] public string responseMessage { get; set; }
+    }
+}
---
+++ RestServiceImpl.svc.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using System.ServiceModel;
+using System.Text;
+
+namespace RestService
+{
+    public class RestServiceImpl : IRestServiceImpl
+    {
+        #region IRestService Members
+        /*
+        public string XMLData(string id)
+        {
+            return "You requested product " + id;
+        }
+
+        public string JSONData(string id)
+        {
+            return "You requested product " + id;
+        }
+        */
+
+        public PCHEmailAPIResponse Pulse()
+        {
+            PCHEmailAPIResponse response = new PCHEmailAPIResponse();
+            response.responseMessage = "Success";
+            response.responseCode = 1;
+            return response;
+        }
+
+        public PCHEmailAPIResponse Preview(PCHEmailAPI request)
+        {
+            PCHEmailAPIResponse response = new PCHEmailAPIResponse();
+            response.responseCode = 1;
+            response.responseMessage = "Success";
+            return response;
+        }
+
+        public PCHEmailAPIResponse Send(PCHEmailAPI request)
+        {
+            PCHEmailAPIResponse response = new PCHEmailAPIResponse();
+            response.responseCode = 1;
+            response.responseMessage = "Success";
+            return response;
+        }
+
+        #endregion
+    }
+}
